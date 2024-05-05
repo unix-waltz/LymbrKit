0354ace62f3389f86144a744054eebc91e746c6a
unix-waltz <riosraskaa@gmail.com>
2024-05-05 13:39:03 +0700

    test case

--- server/src/Controllers/authContoller.js
+++ server/src/Controllers/authContoller.js
@@ -1,4 +1,8 @@
 import { PrismaClient } from '@prisma/client'
+
+
+
+
 import response from '../Utils/response.js'
 import jwtSign from '../Utils/jwtSign.js'
 import setCookie from '../Utils/setCookie.js'
