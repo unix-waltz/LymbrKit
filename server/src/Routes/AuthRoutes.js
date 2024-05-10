ee2443018a8aff40ddf2b870935caf23173f1a1d
unix-waltz <riosraskaa@gmail.com>
2024-05-10 14:20:35 +0700

    ( Finish )<refresh-token> : Create & configure Refresh Route

--- server/src/Routes/AuthRoutes.js
+++ server/src/Routes/AuthRoutes.js
@@ -4,5 +4,6 @@ import authController from "../Controllers/authContoller.js";
 const Auth = Router()
 Auth.post('/register',authMiddleware.isNotAuth,authController.Register)
 Auth.post('/login',authMiddleware.isNotAuth,authController.Login)
-Auth.post('/login',authMiddleware.isAuth('none'),authController.Logout)
+Auth.post('/logout',authMiddleware.isAuth('none'),authController.Logout)
+Auth.post('/refresh',authMiddleware.isAuth('none'),authController.Refresh)
 export default Auth
